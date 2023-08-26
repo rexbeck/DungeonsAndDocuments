1a78d62873f8a8dea4d229a524f98dc93b86398c
rexbeck <reaganrbeck@gmail.com>
2023-08-26 13:32:12 -0400

    init

---
+++ DungeonsAndDocuments/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DungeonsAndDocuments.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
